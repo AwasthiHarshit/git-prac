8d1592d9bce4a1a782b05d33b4f526df558349b3
Harshit Awasthi <harshit.awa@gmail.com>
2022-10-23 18:58:31 +0530

    Revert "test commit 1+2+3"
    This reverts commit c4c43e13e62497414eedf248e5313b6a1cf2e4d5.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world from dev 3 branch");
         System.out.println("new line from main");
-        System.out.println("new line from test commit 3");
+        System.out.println("new line from main 1-2");
     }
 }
