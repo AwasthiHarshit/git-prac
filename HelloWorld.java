8543ddf4b040f96c3ef3002520dd7a4331aab3af
Harshit Awasthi <harshit.awa@gmail.com>
2022-10-23 18:33:57 +0530

    dev3 branch creating conflicts resolved.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello world from main branch");
+        System.out.println("Hello world from dev 3 branch");
         System.out.println("new line from main");
         System.out.println("new line from main 1-2");
     }
