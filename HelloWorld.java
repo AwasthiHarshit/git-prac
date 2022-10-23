2b975518c350037eb5d00679026f12c29c9d3565
AwasthiHarshit <97830081+AwasthiHarshit@users.noreply.github.com>
2022-10-23 18:34:32 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world from main branch");
         System.out.println("new line from main");
-        System.out.println("new line from main 2");
+        System.out.println("new line from main 1-2");
     }
 }
