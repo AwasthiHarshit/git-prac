c1ba06cb24981b03fce7e0edaba78b0eb0666e31
Harshit Awasthi <harshit.awa@gmail.com>
2022-10-23 17:40:40 +0530

    creating dev3 branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello world from dev 2 branch");
+        System.out.println("Hello world from dev 3 branch");
         System.out.println("new line from main");
     }
 }
